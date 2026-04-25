c558cc032ba708c471718ff81112f31021237a44
Daniel <daniel.odontolegal@gmail.com>
2026-04-25 11:19:16 -0300

    Atualiza o LGPD.py
    Atualiza o código para realização da atividade 1, além de padronizar os comentários e aspas duplas do professor para o jeito que costumo utilizar (aspas simples) para padronizar todo o código.

--- LGPD.py
+++ LGPD.py
@@ -3,19 +3,39 @@ from datetime import datetime
 
 import time
 from functools import wraps
-def medir_tempo(func):
-    """Decorator que mede o tempo de execução de uma função."""
+def medir_tempo(func): # decorator que mede o tempo de execução de uma função.
     @wraps(func)
     def wrapper(*args, **kwargs):
         inicio = time.perf_counter()  # tempo inicial (mais preciso que time.time)
         resultado = func(*args, **kwargs)
         fim = time.perf_counter()     # tempo final
         duracao = fim - inicio
-        print(f"⏱ Função '{func.__name__}' executada em {duracao:.6f} segundos.")
+        print(f'⏱ Função "{func.__name__}" executada em {duracao:.6f} segundos.')
         return resultado
     return wrapper
 
-engine = create_engine("postgresql+psycopg2://alunos:AlunoFatec@200.19.224.150:5432/atividade2", echo=False)
+class Usuario: # cria a classe usuário para tratar os dados do banco (abstração)
+    def __init__(self, row):
+        self.id = row[0]
+        self.nome = row[1]
+        self.cpf = row[2]
+        self.email = row[3]
+        self.telefone = row[4]
+        self.data_nascimento = row[5]
+
+    def anonimizar(self): # encapsulamento para lógica de tratamento da atividade 1 (anonimização)
+        partes = self.nome.split(' ') # nome (ex: O***** Araújo)
+        primeiro = partes[0]
+        self.nome = primeiro[0] + ('*' * (len(primeiro)-1)) + ' ' + ' '.join(partes[1:])
+        
+        self.cpf = f'{self.cpf[:3]}.***.***-**' # CPF (ex: 237.***.***-**)
+        
+        user, domain = self.email.split('@') # e-mail (ex: n*********@example.com)
+        self.email = f'{user[0]}{'*' * 9}@{domain}'
+        
+        self.telefone = f'+** (***) *****-{self.telefone[-4:]}' # telefone (ex: +** (***) *****-6810)
+
+engine = create_engine('postgresql+psycopg2://alunos:AlunoFatec@200.19.224.150:5432/atividade2', echo=False)
 metadata = MetaData()
 
 usuarios = Table(
@@ -33,15 +53,17 @@ usuarios = Table(
 metadata.create_all(engine)
 
 @medir_tempo
-def LGPD(row):
-    return row
+def LGPD(row): # transforma a row em um objeto e anonimiza para a atividade 1
+    user_obj = Usuario(row) # instanciação
+    user_obj.anonimizar()    # chamada do método
+    return user_obj
 
 users = []
 with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM usuarios LIMIT 5;"))
+    result = conn.execute(text('SELECT * FROM usuarios LIMIT 5;'))
     for row in result:
         row = LGPD(row)
         users.append(row)
 
-for user in users:
-    print(user)
+for user in users: # ver os dados anonimizados no terminal
+    print(f'Nome: {user.nome} \nCPF: {user.cpf} \nEmail: {user.email} \nTel: {user.telefone}')
