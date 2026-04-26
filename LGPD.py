0644d26d0c77282cc01b1415cfb1dd7ece7202df
Daniel <daniel.odontolegal@gmail.com>
2026-04-26 10:49:40 -0300

    Atualiza o LGPD.py
    Implementa exportação (.xlsx) por ano da atividade 2.

--- LGPD.py
+++ LGPD.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, DateTime, insert, text
 from datetime import datetime
-
 import time
 from functools import wraps
+import os
+import pandas as pd
+
+if not os.path.exists('data'): # cria a pasta data se n existir
+    os.makedirs('data')
+
 def medir_tempo(func): # decorator que mede o tempo de execução de uma função.
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -52,18 +57,47 @@ usuarios = Table(
 
 metadata.create_all(engine)
 
-@medir_tempo
 def LGPD(row): # transforma a row em um objeto e anonimiza para a atividade 1
     user_obj = Usuario(row) # instanciação
     user_obj.anonimizar()    # chamada do método
     return user_obj
 
+@medir_tempo
+def atividade_2(lista_usuarios): # exportar os dados anonimizados em arquivos XLS separados por ano
+    grupos_por_ano = {} # cria um dicionário para agrupar por ano: { 1990: [dados], 1991: [dados] }
+
+    for user in lista_usuarios:
+        ano = user.data_nascimento.year
+        
+        dados = { # cria o dicionário com os dados que vão para a planilha
+            'ID': user.id,
+            'Nome': user.nome,
+            'CPF': user.cpf,
+            'Email': user.email,
+            'Telefone': user.telefone,
+            'Data de Nascimento': user.data_nascimento
+        }
+
+        if ano not in grupos_por_ano: # verifica se o grupo do ano já foi criado
+            grupos_por_ano[ano] = [] # se o ano ainda não existia, cria ele
+        
+        grupos_por_ano[ano].append(dados) # localiza o grupo do ano correspondente e guarda na lista dele
+
+    for ano, lista_dados in grupos_por_ano.items(): # gera um arquivo XLS para cada ano usando Pandas
+        df = pd.DataFrame(lista_dados)
+        caminho = f'data/{ano}.xlsx'
+        df.to_excel(caminho, index=False)
+    
+    print(f'Atividade 2: {len(grupos_por_ano)} arquivos gerados na pasta data.')
+
 users = []
 with engine.connect() as conn:
-    result = conn.execute(text('SELECT * FROM usuarios LIMIT 5;'))
+    result = conn.execute(text('SELECT * FROM usuarios;'))
     for row in result:
         row = LGPD(row)
         users.append(row)
 
-for user in users: # ver os dados anonimizados no terminal
+for user in users[:5]: # ver apenas os 5 primeiros dados anonimizados no terminal para conferência da atividade 1
     print(f'Nome: {user.nome} \nCPF: {user.cpf} \nEmail: {user.email} \nTel: {user.telefone}')
+
+atividade_2(users) # chama a função para gerar os arquivos por ano
